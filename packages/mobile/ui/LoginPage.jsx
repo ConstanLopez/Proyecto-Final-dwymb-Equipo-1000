72c7d067c9919565a391c5e23e89545b648bac3a
ConstanLopez <constantino.lopez@correo.ucu.edu.uy>
2025-11-17 10:36:54 -0300

    Creacion pagina login

---
+++ packages/mobile/ui/LoginPage.jsx
@@ -0,0 +1,11 @@
+import {View,Text,Button} from 'react-native'
+export default function Login () {
+
+    return (
+        <View>
+            <Text>Welcome to PencApp</Text>
+            <Button title="Login" />
+            <Button title="Sign up" />
+        </View>
+    )
+}
